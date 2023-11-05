9d07d25e0571760371c56d90fea1b69f9dc7c513
Delroy Robinson <robinsond993@gmail.com>
2023-11-04 19:41:26 -0500

    Completed Project

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
-/* Add your JavaScript to this file */
+window.addEventListener('load',()=>{
+    let form=document.querySelector("form");
+    let email=document.querySelector("#email");
+    let message=document.querySelector(".message");
+
+    form.addEventListener("submit",function(event){
+        event.preventDefault();
+
+        let useremail=email.value.trim();
+
+        if(useremail===""){
+            message.innerHTML="Please enter a valid email address.";
+        }
+        else{
+            message.innerHTML='Thank you! Your email address '+useremail+' has been added to out mailing list!';
+
+        }
+    });
+});
